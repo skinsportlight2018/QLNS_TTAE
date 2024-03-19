e22b855957b1c646941159db63533bc939d6f483
skinsportlight2018 <skinsportlight2018@gmail.com>
2024-03-19 22:03:11 +0700

    capnhat giao dien ban 1.0

---
+++ Giaodiendemo/layouts/sidebar.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Quản lý nhân sự American English</title>
+
+	<!-- Custom fonts for this template-->
+	<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template-->
+	<link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+	<!-- Page Wrapper -->
+    <div id="wrapper">
+
+		<!-- Sidebar -->
+		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion;" id="accordionSidebar">
+		<div style="background-color: #2C3D57;">
+			<!-- Sidebar - Brand -->
+			<div style="background-color: #2C3D57;"> 
+				<a class="sidebar-brand d-flex align-items-center justify-content-center" href="main.php" style="margin-bottom: 50px;">
+					<div style="display: flex; justify-content: space-between; align-items: center; margin-top: 40px;">
+						<div style="text-transform: uppercase; font-size: 80px; font-family: 'Sorts Mill Goudy', serif; margin-top: 20px;">AE</div>
+						<div style="font-size: 9px; text-align: left; margin-left: 5px; color: white;">the &star; American English School in An Giang</div>
+					</div>
+				</a>
+			</div>
+			
+			<!-- Bảng lề trái quản lý nhân viên-->
+			
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Dashboard -->
+				<li class="nav-item active">
+					<a class="nav-link" href="main.php">
+						<i class="fas fa-fw fa-tachometer-alt"></i>
+						<span>Tổng quan</span></a>
+				</li>
+
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Pages Collapse Menu -->
+				<li class="nav-item">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseNhanVien" aria-expanded="true" aria-controls="collapseNhanVien">
+						<i class="bi bi-person-fill"></i>
+						<span>Nhân viên</span>
+					</a>
+					<div id="collapseNhanVien" class="collapse" aria-labelledby="headingNhanVien" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<a class="collapse-item" href="">Danh sách nhân viên</a>
+							<a class="collapse-item" href="">Phòng ban</a>
+							<a class="collapse-item" href="">Loại nhân viên</a>
+							<a class="collapse-item" href="">Chức vụ</a>
+							<a class="collapse-item" href="">Trình độ</a>
+							<a class="collapse-item" href="">Chuyên môn</a>
+							<a class="collapse-item" href="">Bằng cấp</a>
+						</div>
+					</div>
+				</li>
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Utilities Collapse Menu -->
+				<li class="nav-item">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLuong" aria-expanded="true" aria-controls="collapseLuong">
+						<i class="bi bi-cash-coin"></i>
+						<span>Lương</span>
+					</a>
+					<div id="collapseLuong" class="collapse" aria-labelledby="headingLuong" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<a class="collapse-item" href="">Bảng tính lương</a>
+							<a class="collapse-item" href="">Tính lương</a>
+
+						</div>
+					</div>
+				</li>
+
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Pages Collapse Menu -->
+				<li class="nav-item">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+						<i class="bi bi-person-bounding-box"></i>
+						<span>Công tác</span>
+					</a>
+					<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<a class="collapse-item" href="">Danh sách công tác</a>
+							<a class="collapse-item" href="">Tạo công tác</a>
+						</div>
+					</div>
+				</li>
+				
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Charts -->
+				<li class="nav-item">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseGroup" aria-expanded="true" aria-controls="collapseGroup">
+						<i class="bi bi-people-fill"></i>
+						<span>Nhóm nhân viên</span>
+					</a>
+					<div id="collapseGroup" class="collapse" aria-labelledby="headingGroup" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<a class="collapse-item" href="">Danh sách nhóm</a>
+							<a class="collapse-item" href="">Tạo nhóm</a>
+						</div>
+					</div>
+				</li>
+
+				<!-- Divider -->
+				<hr class="sidebar-divider my-0">
+
+				<!-- Nav Item - Tables -->
+				<li class="nav-item">
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAccount" aria-expanded="true" aria-controls="collapseAccount">
+						<i class="bi bi-gear-fill"></i>
+						<span>Tài khoản</span>
+					</a>
+					<div id="collapseAccount" class="collapse" aria-labelledby="headingAccount" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<a class="collapse-item" href="">Danh tài khoản</a>
+							<a class="collapse-item" href="">Tạo tài khoản</a>
+						</div>
+					</div>
+				</li>
+			
+	
+				<!-- Divider -->
+				<hr class="sidebar-divider d-none d-md-block">
+
+				<!-- Sidebar Toggler (Sidebar) -->
+				<div class=" d-none d-md-inline"style="margin-left: 30px;">
+					<button class="rounded-circle border-0" id="sidebarToggle"></button>
+				</div>
+
+
+			</ul>
+	
+	<!-- End of Sidebar -->
+
--- Giaodiendemo/layouts/top.php
+++ Giaodiendemo/layouts/top.php
@@ -1,160 +1,192 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<!-- Content Wrapper -->
 <head>
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<title>Quản lý nhân sự American English</title>
-
-	<!-- Custom fonts for this template-->
-	<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-	
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
-
-	<!-- Custom styles for this template-->
-	<link href="../css/sb-admin-2.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
 </head>
 
-<body id="page-top">
+<div id="content-wrapper" class="d-flex flex-column">
 
-	<!-- Page Wrapper -->
-    <div id="wrapper">
+    <!-- Main Content -->
+    <div id="content">
 
-		<!-- Sidebar -->
-		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion;" id="accordionSidebar">
-		<div style="background-color: #2C3D57;">
-			<!-- Sidebar - Brand -->
-			<div style="background-color: #2C3D57;"> 
-				<a class="sidebar-brand d-flex align-items-center justify-content-center" href="main.php" style="margin-bottom: 50px;">
-					<div style="display: flex; justify-content: space-between; align-items: center; margin-top: 40px;">
-						<div style="text-transform: uppercase; font-size: 80px; font-family: 'Sorts Mill Goudy', serif; margin-top: 20px;">AE</div>
-						<div style="font-size: 9px; text-align: left; margin-left: 5px; color: white;">the &star; American English School in An Giang</div>
-					</div>
-				</a>
-			</div>
-			
-			<!-- Bảng lề trái quản lý nhân viên-->
-			
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-				<!-- Nav Item - Dashboard -->
-				<li class="nav-item active">
-					<a class="nav-link" href="index.html">
-						<i class="fas fa-fw fa-tachometer-alt"></i>
-						<span>Tổng quan</span></a>
-				</li>
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
 
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+            <!-- Topbar Search -->
+            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
 
-				<!-- Nav Item - Pages Collapse Menu -->
-				<li class="nav-item">
-					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseNhanVien" aria-expanded="true" aria-controls="collapseNhanVien">
-						<i class="bi bi-person-fill"></i>
-						<span>Nhân viên</span>
-					</a>
-					<div id="collapseNhanVien" class="collapse" aria-labelledby="headingNhanVien" data-parent="#accordionSidebar">
-						<div class="bg-white py-2 collapse-inner rounded">
-							<a class="collapse-item" href="">Danh sách nhân viên</a>
-							<a class="collapse-item" href="">Phòng ban</a>
-							<a class="collapse-item" href="">Loại nhân viên</a>
-							<a class="collapse-item" href="">Chức vụ</a>
-							<a class="collapse-item" href="">Trình độ</a>
-							<a class="collapse-item" href="">Chuyên môn</a>
-							<a class="collapse-item" href="">Bằng cấp</a>
-						</div>
-					</div>
-				</li>
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
 
-				<!-- Nav Item - Utilities Collapse Menu -->
-				<li class="nav-item">
-					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLuong" aria-expanded="true" aria-controls="collapseLuong">
-						<i class="bi bi-cash-coin"></i>
-						<span>Lương</span>
-					</a>
-					<div id="collapseLuong" class="collapse" aria-labelledby="headingLuong" data-parent="#accordionSidebar">
-						<div class="bg-white py-2 collapse-inner rounded">
-							<a class="collapse-item" href="">Bảng tính lương</a>
-							<a class="collapse-item" href="">Tính lương</a>
+                <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                <li class="nav-item dropdown no-arrow d-sm-none">
+                    <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-search fa-fw"></i>
+                    </a>
+                    <!-- Dropdown - Messages -->
+                    <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                        <form class="form-inline mr-auto w-100 navbar-search">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small" placeholder="Nhập thông tin cần tìm..." aria-label="Search" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
 
-						</div>
-					</div>
-				</li>
+                <!-- Nav Item - Alerts -->
+                <li class="nav-item dropdown no-arrow mx-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-bell fa-fw"></i>
+                        <!-- Counter - Alerts -->
+                        <span class="badge badge-danger badge-counter">3+</span>
+                    </a>
+                    <!-- Dropdown - Alerts -->
+                    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                        <h6 class="dropdown-header">
+                            Alerts Center
+                        </h6>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="mr-3">
+                                <div class="icon-circle bg-primary">
+                                    <i class="fas fa-file-alt text-white"></i>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="small text-gray-500">December 12, 2019</div>
+                                <span class="font-weight-bold">A new monthly report is ready to download!</span>
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="mr-3">
+                                <div class="icon-circle bg-success">
+                                    <i class="fas fa-donate text-white"></i>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="small text-gray-500">December 7, 2019</div>
+                                $290.29 has been deposited into your account!
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="mr-3">
+                                <div class="icon-circle bg-warning">
+                                    <i class="fas fa-exclamation-triangle text-white"></i>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="small text-gray-500">December 2, 2019</div>
+                                Spending Alert: We've noticed unusually high spending for your account.
+                            </div>
+                        </a>
+                        <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                    </div>
+                </li>
 
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+                <!-- Nav Item - Messages -->
+                <li class="nav-item dropdown no-arrow mx-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-envelope fa-fw"></i>
+                        <!-- Counter - Messages -->
+                        <span class="badge badge-danger badge-counter">7</span>
+                    </a>
+                    <!-- Dropdown - Messages -->
+                    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                        <h6 class="dropdown-header">
+                            Message Center
+                        </h6>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="dropdown-list-image mr-3">
+                                <img src="../img/undraw_profile_1.svg" alt="...">
+                                <div class="status-indicator bg-success"></div>
+                            </div>
+                            <div class="font-weight-bold">
+                                <div class="text-truncate">Hi there! I am wondering if you can help me with a
+                                    problem I've been having.</div>
+                                <div class="small text-gray-500">Emily Fowler · 58m</div>
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="dropdown-list-image mr-3">
+                                <img class="rounded-circle" src="../img/undraw_profile_2.svg" alt="...">
+                                <div class="status-indicator"></div>
+                            </div>
+                            <div>
+                                <div class="text-truncate">I have the photos that you ordered last month, how
+                                    would you like them sent to you?</div>
+                                <div class="small text-gray-500">Jae Chun · 1d</div>
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="dropdown-list-image mr-3">
+                                <img class="rounded-circle" src="../img/undraw_profile_3.svg" alt="...">
+                                <div class="status-indicator bg-warning"></div>
+                            </div>
+                            <div>
+                                <div class="text-truncate">Last month's report looks great, I am very happy with
+                                    the progress so far, keep up the good work!</div>
+                                <div class="small text-gray-500">Morgan Alvarez · 2d</div>
+                            </div>
+                        </a>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <div class="dropdown-list-image mr-3">
+                                <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="...">
+                                <div class="status-indicator bg-success"></div>
+                            </div>
+                            <div>
+                                <div class="text-truncate">Am I a good boy? The reason I ask is because someone
+                                    told me that people say this to all dogs, even if they aren't good...</div>
+                                <div class="small text-gray-500">Chicken the Dog · 2w</div>
+                            </div>
+                        </a>
+                        <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                    </div>
+                </li>
 
-				<!-- Nav Item - Pages Collapse Menu -->
-				<li class="nav-item">
-					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-						<i class="bi bi-person-bounding-box"></i>
-						<span>Công tác</span>
-					</a>
-					<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-						<div class="bg-white py-2 collapse-inner rounded">
-							<a class="collapse-item" href="">Danh sách công tác</a>
-							<a class="collapse-item" href="">Tạo công tác</a>
-						</div>
-					</div>
-				</li>
-				
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+                <div class="topbar-divider d-none d-sm-block"></div>
 
-				<!-- Nav Item - Charts -->
-				<li class="nav-item">
-					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseGroup" aria-expanded="true" aria-controls="collapseGroup">
-						<i class="bi bi-people-fill"></i>
-						<span>Nhóm nhân viên</span>
-					</a>
-					<div id="collapseGroup" class="collapse" aria-labelledby="headingGroup" data-parent="#accordionSidebar">
-						<div class="bg-white py-2 collapse-inner rounded">
-							<a class="collapse-item" href="">Danh sách nhóm</a>
-							<a class="collapse-item" href="">Tạo nhóm</a>
-						</div>
-					</div>
-				</li>
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Trung tâm ngoại ngữ American English</span>
+                        <img class="img-profile rounded-circle" src="../img/Logo/LogoAE1.jpg" alt="...">
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="login.php">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Thông tin
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Cài đặt
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="login.php" data-toggle="modal" data-target="#logoutModal">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Đăng xuất
+                        </a>
+                    </div>
+                </li>
 
-				<!-- Divider -->
-				<hr class="sidebar-divider my-0">
+            </ul>
 
-				<!-- Nav Item - Tables -->
-				<li class="nav-item">
-					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAccount" aria-expanded="true" aria-controls="collapseAccount">
-						<i class="bi bi-gear-fill"></i>
-						<span>Tài khoản</span>
-					</a>
-					<div id="collapseAccount" class="collapse" aria-labelledby="headingAccount" data-parent="#accordionSidebar">
-						<div class="bg-white py-2 collapse-inner rounded">
-							<a class="collapse-item" href="">Danh tài khoản</a>
-							<a class="collapse-item" href="">Tạo tài khoản</a>
-						</div>
-					</div>
-				</li>
-			
-	
-				<!-- Divider -->
-				<hr class="sidebar-divider d-none d-md-block">
-
-				<!-- Sidebar Toggler (Sidebar) -->
-				<div class=" d-none d-md-inline"style="margin-left: 30px;">
-					<button class="rounded-circle border-0" id="sidebarToggle"></button>
-				</div>
-
-
-			</ul>
-	
-	<!-- End of Sidebar -->
-
+        </nav>
+        <!-- End of Topbar -->
